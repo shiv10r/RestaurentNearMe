9c1bcdf8f712a4c6c1c7406ffe048f4a600ed094
Shivanshu Rai <shr1030cd@gmail.com>
2023-10-01 17:04:16 +0530

    login  function

--- restro/src/app/login/login.component.ts
+++ restro/src/app/login/login.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validator, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponentn implements OnInit {
 
+  formValue!:FormGroup
+
+  constructor(private fb: FormBuilder,private http: HttpClient, private router: Router) { }
+ 
+  ngOnInit(): void {
+
+    this.formValue = this.fb.group({  
+      mobile: ['', Validators.required],
+      passord: ['', Validators.required],
+    });
+
+  }
+
+  //make method to sign user :  
+  login() {
+    this.http.get<any>("http://localhost:4200/signup").subscribe(res => {
+      const  user =  res.find((a:any) =>{
+        return a.email === this.formValue.value.email && a.passord === this.formValue.value.passord
+      });
+      if(user){
+        alert("user  successful login")
+      }else{
+        alert("user  not  found");
+      }
+    },error =>{
+        alert ("Something  wnet wrong")
+
+      })
+  }
 }
+

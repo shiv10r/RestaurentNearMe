ccb89970cb075569a4488759983e278ca086f207
Shivanshu Rai <shr1030cd@gmail.com>
2023-10-01 16:35:58 +0530

    signup functionality

--- restro/src/app/signup/signup.component.ts
+++ restro/src/app/signup/signup.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validator, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
+  myForm !: FormGroup;
+
+  constructor(private fb: FormBuilder,private http: HttpClient, private router: Router) { }
+  ngOnInit(): void {
+
+    this.myForm = this.fb.group({   //why this  gesture :
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      mobile: ['', Validators.required],
+      passord: ['', Validators.required],
+    });
+
+  }
+
+  //make method to create  user :  
+  signup() {
+    this.http.post<any>("http://localhost:4200/signup", this.myForm.value).subscribe(res => {
+      alert("you are registered successfully!");
+      this.myForm.reset();
+      this.router.navigate(['login'])
+    }, error => {
+      alert("Something went wrong!!!");
+    })
+  }
 }
+
